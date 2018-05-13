561980a26ca82975c3e8cf7acd275809e7f01dcf
Matthias Totschnig <matthias@totschnig.org>
2018-05-13 18:12:29 +0200

    Add `new` subcommand
    That currently does not do anything

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 extern crate clap;
-use clap::App;
+use clap::{App, Arg, SubCommand};
 
 fn main() {
     App::new("workspace")
         .version("0.0.0")
         .about("Manages workspaces for all your projects!")
         .author("Matthias T. and Roma B.")
+        .subcommand(
+            SubCommand::with_name("new")
+                .about("Creates a new workspace in this directory")
+                .arg(
+                    Arg::with_name("NAME")
+                        .help("Name of the new workspace")
+                        .required(true),
+                ),
+        )
         .get_matches();
 }
