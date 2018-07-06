408890da0494c4c945589ed660c798558b21b4ef
Matthias Totschnig <matthias@totschnig.org>
2018-07-06 20:58:57 +0200

    Add error!, warn! and log! macros

--- src/macros.rs
+++ src/macros.rs
@@ -3,3 +3,25 @@ macro_rules! run {
         println!(concat!("RUN>", $command)$(, $arg)*);
     };
 }
+
+macro_rules! error {
+    ($message:expr$(,$arg:expr)*) => {
+        eprintln!(concat!("{} ", $message), "error:".red().bold()$(, $arg)*);
+    };
+}
+
+macro_rules! warn {
+    ($message:expr$(,$arg:expr)*) => {
+        println!(concat!("{} ", $message), "warning:".yellow().bold()$(, $arg)*);
+    };
+}
+
+macro_rules! log {
+    ($message:expr$(,$arg:expr)*) => {
+        unsafe {
+            if VERBOSE {
+                println!(concat!("{} ", $message), "info:".bright_blue().bold()$(, $arg)*);
+            }
+        }
+    };
+}
