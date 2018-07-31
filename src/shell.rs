229fb59eb8d71f1a4f265eee0984caee6c484533
Matthias Totschnig <matthias@totschnig.org>
2018-07-31 16:07:28 +0200

    Use rawstrings in src/shell.rs

--- src/shell.rs
+++ src/shell.rs
@@ -1,34 +1,34 @@
-pub const BASH: &str = "function ws {
+pub const BASH: &str = r#"function ws {
     while read -r line; do
-        if [[ \"$line\" == RUN\\>* ]]; then
-            eval \"${line:4}\";
+        if [[ "$line" == RUN\>* ]]; then
+            eval "${line:4}";
         else
-            echo \"$line\";
+            echo "$line";
         fi;
-    done < <( workspace \"$@\" );
-}";
+    done < <( workspace "$@" );
+}"#;
 
-pub const POWERSHELL: &str = "function ws {
+pub const POWERSHELL: &str = r#"function ws {
     workspace $args | % {
-        if ($_ -match \"^RUN>\") {
+        if ($_ -match "^RUN>") {
             . ([scriptblock]::Create($_.Substring(4)))
         } else {
             Write-Output $_
         }
     }
-}";
+}"#;
 
-pub const CMD: &str = "@ECHO off
-FOR /F \"tokens=* delims=\" %%G IN ('workspace %*') DO (
-    CALL :subroutine \"%%G\"
+pub const CMD: &str = r#"@ECHO off
+FOR /F "tokens=* delims=" %%G IN ('workspace %*') DO (
+    CALL :subroutine "%%G"
 )
 GOTO :EOF
 
 :subroutine
-    SET \"temp=%~1\"
-    IF \"%temp:~0,4%\" == \"RUN>\" (
+    SET "temp=%~1"
+    IF "%temp:~0,4%" == "RUN>" (
         CALL %temp:~4%
     ) ELSE (
         ECHO %~1
     )
-    GOTO :EOF";
+    GOTO :EOF"#;
